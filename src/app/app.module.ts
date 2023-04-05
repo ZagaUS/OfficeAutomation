99fdc6db709f58b2de13d25c7c543cf5e6eb50cf
Harinolon <hariduke001@gmail.com>
2023-04-05 17:42:38 +0530

    dashboard created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectDashboardComponent } from './projectModule/project-dashboard/project-dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent, ProjectDashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/projectModule/project-dashboard/project-dashboard.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  projectId: string;
+  projectName: String;
+  employeeName: String;
+  role: String;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    projectId: '1',
+    projectName: 'citioverdraft',
+    employeeName: 'employee',
+    role: 'Consultant',
+  },
+  {
+    projectId: '2',
+    projectName: 'citi ci',
+    employeeName: 'Employee',
+    role: 'Consultant',
+  },
+];
+
+@Component({
+  selector: 'app-project-dashboard',
+  templateUrl: './project-dashboard.component.html',
+  styleUrls: ['./project-dashboard.component.scss'],
+})
+export class ProjectDashboardComponent {
+  displayedColumns: string[] = [
+    'projectId',
+    'projectName',
+    'employeeName',
+    'role',
+    'action',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log('filterValue', filterValue);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  test() {
+    alert('test');
+    console.log('test');
+  }
+}
