19429355fbc61c64ae311c6fed72c0a1fab3488e
Anushiya <anushiyadhamu@gmail.com>
2023-04-10 15:59:17 +0530

    updated the datepick

--- src/app/project-module/modaldailyweek/modaldailyweek.component.ts
+++ src/app/project-module/modaldailyweek/modaldailyweek.component.ts
@@ -12,21 +12,17 @@ export class ModaldailyweekComponent {
 constructor(
   private router: Router, 
   public modalRef: MdbModalRef<ModaldailyweekComponent>){} 
-  // dailyClicked = false;
-  // externalClicked=false;
-  // hideDailyFields?:any;
-  // hideExternalFields?: any;
-  // dailyFields = ['task1','task2']
   showDaily = false;
   showExternal = false;
   projectName ?: string;
   duration ?: string;
   description ?: string;
   date?: string;
-  task1?: string;
-  task2?: string;
-  task3?: string;
-  task4?: string;
+  hours?: string;
+  upload?: string;
+  startDate?: string;
+  endDate?: string;
+ 
   
   close() {
     const closeMessage = 'Modal closed'
@@ -48,37 +44,22 @@ constructor(
   onClickDaily() {
     this.showDaily = true;
     this.showExternal = false;
-    // this.dailyClicked = true;
-    // this.hideExternalFields=true;
-    // const closeMessage = 'Modal closed';
-    // this.modalRef.close(closeMessage);
-    // this.router.navigate([
-    //     '/dailytimesheet'
-    //   ]);
   }
   onClickExternalSave()
   {
+    const exdata = {
+      upload: this.upload,
+      hours: this.hours,
+      startDate: this.startDate,
+      endDate: this.endDate
+    };
+    console.log(exdata);
     const closeMessage = 'Modal closed';
     this.modalRef.close(closeMessage);
   }
   onClickExternal() {
     this.showDaily = false;
     this.showExternal = true;
-    // this.externalClicked = true;
-    // this.hideDailyFields= true;
-    // const closeMessage = 'Modal closed';
-    // this.modalRef.close(closeMessage);
-    // this.router.navigate([
-    //   '/weeklytimesheet'
-    //     ]);
-    // this.apiCall.sendMail(this.contactForm.value).subscribe((data:any) => {
-    //   console.log(data);
-    //   this.snackBar.open("Thank you for contacting us. We will get back to you soon!", "", {
-    //     duration: this.durationInSeconds * 1000});
-    //   this.contactForm.reset();
-    // }, (error) => {
-    //   console.log(error);
-    // });
-  }
+   }
   
 }
