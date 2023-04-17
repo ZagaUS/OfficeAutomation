259251a0427b91d9ae78e0c53015bd1636fd58cb
Anushiya <anushiyadhamu@gmail.com>
2023-04-17 10:56:20 +0530

    Updated the employeeOnboading Pages

--- src/app/employee-onboarding/job-history/job-history.component.ts
+++ src/app/employee-onboarding/job-history/job-history.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-job-history',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-history.component.scss']
 })
 export class JobHistoryComponent {
-
+  myForm!: FormGroup;
+  isReadOnly = true;
+  
+  constructor(private fb: FormBuilder) { 
+    
 }
+ngOnInit() {
+  // this.http.get('/api/employee').subscribe((employee: any) => {
+  // Use the response to set the initial values of the form fields
+  this.myForm = this.fb.group({
+  employeeName: ['Anushiya'],
+  employeeId: ['1234'],
+  employeeRole: ['Software developer'],
+  gender: ['Female'],
+  education: ['Bachelor of Science'],
+  yearsOfExperience: [1],
+  dateOfJoining: ['2022-01-01']
+}); 
+}  
+//  });
+
+onEdit() {
+  this.isReadOnly = false;
+}
+
+  
+  onSubmit() {
+    this.isReadOnly = false;
+    const formValues = this.myForm.value;
+    console.log(formValues);
+
+    }
+  
+}
+
--- src/app/employee-onboarding/personal-info/personal-info.component.ts
+++ src/app/employee-onboarding/personal-info/personal-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-info',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./personal-info.component.scss']
 })
 export class PersonalInfoComponent {
-
+  myForm!: FormGroup;
+  isReadOnly = true;
+  
+  constructor(private fb: FormBuilder) { 
+    
 }
+ngOnInit() {
+  // this.http.get('/api/employee').subscribe((employee: any) => {
+  // Use the response to set the initial values of the form fields
+  this.myForm = this.fb.group({
+  employeeName: ['Anushiya'],
+  employeeId: ['1234'],
+  employeeRole: ['Software developer'],
+  gender: ['Female'],
+  education: ['Bachelor of Science'],
+  yearsOfExperience: [1],
+  dateOfJoining: ['2022-01-01']
+}); 
+}  
+//  });
+
+onEdit() {
+  this.isReadOnly = false;
+}
+
+  
+  onSubmit() {
+    this.isReadOnly = false;
+    const formValues = this.myForm.value;
+    console.log(formValues);
+
+    }
+  }
