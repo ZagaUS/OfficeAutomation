02ee867c6e8330777d510c8c496f82cbb6824662
Harinolon <hariduke001@gmail.com>
2023-04-30 20:57:43 +0530

    invoice updated

--- src/app/base-api/invoice-api.service.ts
+++ src/app/base-api/invoice-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,13 @@ export class InvoiceApiService {
       this.invoiceUrl + 'createInvoicee/pdf',
       invoiceData
     );
+  }
+
+  getPdf(documentId: any): Observable<Blob> {
+    const headers = new HttpHeaders().set('Accept', 'text/plain');
+    return this.http.get(this.invoiceUrl + `invoice/${documentId}/pdf`, {
+      headers,
+      responseType: 'blob',
+    });
   }
 }
