9c96aaf0e7267f905ea8c53d64e80050009046a3
Clyde Tedrick <iamclyde@gmail.com>
2023-05-28 21:19:41 -0400

    containerization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,8 @@ export const environment = {
   url: 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/zaga/information',
   mailUrl:
     'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/sendmail',
+
+  projectMgtUrl: process.env['PROJECT_MANAGEMENT_URL'],
+  invoiceUrl: process.env['INVOICE_URL'],
+  employeeUrl: process.env['EMPLOYEE_URL'],
 };
