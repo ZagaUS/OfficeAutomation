64f8e973a9d6f78f80377db4c68fd0055f792859
Harinolon <hariduke001@gmail.com>
2023-06-06 21:38:14 +0530

    employee document upload fixed

--- src/app/base-api/employee-api.service.ts
+++ src/app/base-api/employee-api.service.ts
@@ -4,22 +4,17 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeApiService {
-
-
   getweeklyTimesheetfile(document: Document) {
     throw new Error('Method not implemented.');
   }
   employeeURL = environment.employeeUrl;
   constructor(private http: HttpClient) {}
 
-  
-  getEmployeeDetail(){
-    return this.http.get<any>(
-      this.employeeURL + 'getListOfEmployeeInfo'
-    );
+  getEmployeeDetail() {
+    return this.http.get<any>(this.employeeURL + 'getListOfEmployeeInfo');
   }
 
   getListOfDayOffbyId(employeeId: string) {
@@ -28,107 +23,93 @@ export class EmployeeApiService {
     );
   }
 
-  updateDayOff(formValues:any){
-    return this.http.put<any>(
-      this.employeeURL + 'updateDayOff/' ,
-      formValues
+  updateDayOff(formValues: any) {
+    return this.http.put<any>(this.employeeURL + 'updateDayOff/', formValues);
+  }
+
+  getSkillDetailsbyId(employeeId: string) {
+    return this.http.get<any>(
+      this.employeeURL + `getSkillsById/?employeeId=${employeeId}`
     );
   }
 
-  getSkillDetailsbyId(employeeId: string){
-    return this.http.get<any>(
-      this.employeeURL + `getSkillsById/?employeeId=${employeeId}`
-    )
-  }
-
-  updateSkillDetails(employeeId: string, skillValues:any) {
+  updateSkillDetails(employeeId: string, skillValues: any) {
     return this.http.put<any>(
-      this.employeeURL + 'updateSkills/' +employeeId,
+      this.employeeURL + 'updateSkills/' + employeeId,
       skillValues
     );
   }
 
-    getPersonalInfobyId(employeeId: string){
-      return this.http.get<any>(
-        this.employeeURL + `getPersonalInfoById/?employeeId=${employeeId}`
-      );
-    }
+  getPersonalInfobyId(employeeId: string) {
+    return this.http.get<any>(
+      this.employeeURL + `getPersonalInfoById/?employeeId=${employeeId}`
+    );
+  }
 
-    updatePersonalValues(PersonalValues:any){
-      return this.http.put<any>(
-        this.employeeURL + 'updatePersonalInfo/',
-        PersonalValues
-      );
-    }
+  updatePersonalValues(PersonalValues: any) {
+    return this.http.put<any>(
+      this.employeeURL + 'updatePersonalInfo/',
+      PersonalValues
+    );
+  }
 
-    getEmployeeInfobyId(employeeId: string){
-      return this.http.get<any>(
-        this.employeeURL + `getEmployeeInfo?employeeId=${employeeId}`
-      );
-    }
+  getEmployeeInfobyId(employeeId: string) {
+    return this.http.get<any>(
+      this.employeeURL + `getEmployeeInfo?employeeId=${employeeId}`
+    );
+  }
 
-    updateEmployeeInfo(employeeId: string,empValue:any){
-      return this.http.put<any>(
-        this.employeeURL + 'updateEmployeeInfo/' +employeeId,
-        empValue
-      );
-    }
+  updateEmployeeInfo(employeeId: string, empValue: any) {
+    return this.http.put<any>(
+      this.employeeURL + 'updateEmployeeInfo/' + employeeId,
+      empValue
+    );
+  }
 
-    getListOfJobHistory(employeeId: string){
-      return this.http.get<any>(
-        this.employeeURL + `getJobHistoryById/?employeeId=${employeeId}`
-      );
-    }
+  getListOfJobHistory(employeeId: string) {
+    return this.http.get<any>(
+      this.employeeURL + `getJobHistoryById/?employeeId=${employeeId}`
+    );
+  }
 
-    getListOfEducation(employeeId: string){
-      return this.http.get<any>(
-        this.employeeURL + `getEducationDetailsById/?employeeId=${employeeId}`
-      );
-    }
+  getListOfEducation(employeeId: string) {
+    return this.http.get<any>(
+      this.employeeURL + `getEducationDetailsById/?employeeId=${employeeId}`
+    );
+  }
 
-    createEmployee(createEmployeeDetails: any){
-      console.log("created inside api",createEmployeeDetails);
-      return this.http.post<any>(
-        this.employeeURL + 'createEmployee',
-        createEmployeeDetails
-      );
-    }
+  createEmployee(createEmployeeDetails: any) {
+    console.log('created inside api', createEmployeeDetails);
+    return this.http.post<any>(
+      this.employeeURL + 'createEmployee',
+      createEmployeeDetails
+    );
+  }
 
-    deleteEmployee(employeeId: string){
-      return this.http.delete<any>(
-        // this.employeeURL + 'deleteEmployee' + employeeId
-        this.employeeURL + `deleteEmployee?employeeId=${employeeId}`
-      );
-    }
+  deleteEmployee(employeeId: string) {
+    return this.http.delete<any>(
+      // this.employeeURL + 'deleteEmployee' + employeeId
+      this.employeeURL + `deleteEmployee?employeeId=${employeeId}`
+    );
+  }
 
-    downloadPDF(employeeId: string): Observable<Blob> {
-      const headers = new HttpHeaders().set('Accept', 'text/plain');
-      return this.http.get(
-        this.employeeURL + `pdf/${employeeId}`,
-        { headers, responseType: 'blob' }
-      );
-    }
+  downloadPDF(employeeId: string): Observable<Blob> {
+    const headers = new HttpHeaders().set('Accept', 'text/plain');
+    return this.http.get(this.employeeURL + `pdf/${employeeId}`, {
+      headers,
+      responseType: 'blob',
+    });
+  }
 
-    upload(
-      document:any,
-      employeeId:any,
-      name: any,
+  upload(document: File, employeeId: string, name: string): Observable<any> {
+    const metadata = `{ "employeeId": "${employeeId}", "employeeName": "${name}" }`;
+    // const metadata = `{"employeeId":"33","employeeName":"22"}`;
+    const formData = new FormData();
+    console.log(metadata);
+    formData.append('file', document, document.name);
+    formData.append('metadata', metadata);
+    console.log('upload Resume' + ' ' + formData);
 
-      
-    ) {
-      console.log(
-        'upload Resume' +
-          ' ' +
-         name 
-      );
-      return this.http.post(
-        this.employeeURL +
-          `/upload`,
-        document,
-        {
-          headers: { 'Content-Type': 'application/octet-stream' },
-        }
-      );
-    }
-    
+    return this.http.post<any>(this.employeeURL + `upload`, formData);
+  }
 }
--- src/app/employee-onboarding/modal-resumeupload/modal-resumeupload.component.ts
+++ src/app/employee-onboarding/modal-resumeupload/modal-resumeupload.component.ts
@@ -8,20 +8,21 @@ import { EmployeeApiService } from 'src/app/base-api/employee-api.service';
 @Component({
   selector: 'app-modal-resumeupload',
   templateUrl: './modal-resumeupload.component.html',
-  styleUrls: ['./modal-resumeupload.component.scss']
+  styleUrls: ['./modal-resumeupload.component.scss'],
 })
 export class ModalResumeuploadComponent {
   contactForm: FormGroup;
   @ViewChild('fileInput') fileInput?: ElementRef;
+  pdfFile?: any;
 
   constructor(
     private router: Router,
     public modalRef: MdbModalRef<ModalResumeuploadComponent>,
     private api: EmployeeApiService,
     public fb: FormBuilder
-  ){
+  ) {
     this.contactForm = this.fb.group({
-      fileName: ['', Validators.required],
+      fileName: ['dummy.pdf', Validators.required],
       uploadfile: ['', Validators.required],
       // hours: ['', Validators.required],
       // startDate: [new Date(), Validators.required],
@@ -29,73 +30,71 @@ export class ModalResumeuploadComponent {
     });
   }
 
-// dateFormat = 'yyyy-MM-dd';
-fileName: any = '';
+  // dateFormat = 'yyyy-MM-dd';
+  fileName: any = '';
 
-name?: any = localStorage.getItem('employeeName');
-employeeId?: any = localStorage.getItem('employeeId');
+  name?: any = localStorage.getItem('employeeName');
+  employeeId?: any = localStorage.getItem('employeeId');
 
-onFileSelected(event: any) {
-  const files: File = event.target.files[0];
-  // console.log(files.name);
-  this.fileName = files.name;
-  const fileInputElement = event.target;
-  fileInputElement.value = '';
-  this.contactForm.get('uploadfile')?.setValue(files);
-}
+  onFileSelected(event: any) {
+    this.pdfFile = event.target.files[0];
+    // console.log(files.name);
+    // this.fileName = files.name;
+    // this.pdfFile = files;
+    const fileInputElement = event.target;
+    fileInputElement.value = '';
+    // this.contactForm.get('uploadfile')?.setValue(files);
+  }
 
-openFileSelection() {
-  this.fileInput?.nativeElement.click();
-}
+  openFileSelection() {
+    this.fileInput?.nativeElement.click();
+  }
 
+  close() {
+    const closeMessage = 'Modal closed';
+    this.modalRef.close(closeMessage);
+  }
+  onClickExternal() {}
 
-close() {
-  const closeMessage = 'Modal closed';
-  this.modalRef.close(closeMessage);
-}
-onClickExternal(){}
+  onClickExternalSave() {
+    // const startDate = formatDate(
+    //   this.contactForm.value.startDate,
+    //   this.dateFormat,
+    //   'en-US'
+    // );
+    // const endDate = formatDate(
+    //   this.contactForm.value.endDate,
+    //   this.dateFormat,
+    //   'en-US'
+    // );
+    // const docType = 'EXTERNAL';
+    // this.contactForm.get('endDate')?.setValue(endDate);
+    // this.contactForm.get('startDate')?.setValue(startDate);
+    // const projectName = localStorage.getItem('projectName');
+    // const projectId = localStorage.getItem('projectId');
+    // const poId = localStorage.getItem('poId');
+    // const employeeName = localStorage.getItem('employeeName')
+    console.log(this.employeeId + ' is ' + this.name);
+    this.api
+      .upload(this.pdfFile, this.employeeId, this.name)
+      .subscribe((data: any) => {
+        console.log('External Timesheet of projects ' + JSON.stringify(data));
+        // window.location.reload();
+      });
 
-onClickExternalSave() {
-  // const startDate = formatDate(
-  //   this.contactForm.value.startDate,
-  //   this.dateFormat,
-  //   'en-US'
-  // );
-  // const endDate = formatDate(
-  //   this.contactForm.value.endDate,
-  //   this.dateFormat,
-  //   'en-US'
-  // );
-  // const docType = 'EXTERNAL';
-  // this.contactForm.get('endDate')?.setValue(endDate);
-  // this.contactForm.get('startDate')?.setValue(startDate);
-  // const projectName = localStorage.getItem('projectName');
-  // const projectId = localStorage.getItem('projectId');
-  // const poId = localStorage.getItem('poId');
-  // const employeeName = localStorage.getItem('employeeName')
+    console.log(this.contactForm.value);
 
-  this.api.upload(
-      this.contactForm.value,
-      this.employeeId,
-      this.name).subscribe((data:any) => {
-      console.log('External Timesheet of projects ' + JSON.stringify(data));
-      // window.location.reload();
-    });
-
-  console.log(this.contactForm.value);
-
-  const closeMessage = 'Modal closed';
-  this.modalRef.close(closeMessage);
-}
-onSubmit() {
-  // const po = this.contactForm.value;
-  // this.api.createProjectDetails(projectDetails).subscribe((data: any) => {
-  //   console.log('data updated', data);
-  //   alert('Updated successfully');
-  //   // do something with the response, if needed
-  // });
-}
-// onGenerateClick(){}
-onClickDaily(){}
-
+    const closeMessage = 'Modal closed';
+    this.modalRef.close(closeMessage);
+  }
+  onSubmit() {
+    // const po = this.contactForm.value;
+    // this.api.createProjectDetails(projectDetails).subscribe((data: any) => {
+    //   console.log('data updated', data);
+    //   alert('Updated successfully');
+    //   // do something with the response, if needed
+    // });
+  }
+  // onGenerateClick(){}
+  onClickDaily() {}
 }
