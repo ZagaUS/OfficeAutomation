00820de825f88c4640c13db577c3a7d7d8b4e526
Jeya Raghul <sharanya.june7@gmail.com>
2023-06-09 13:49:47 +0530

    Mapped Client Address in Invoice

--- src/app/invoice-module/invoice-module1/invoice-module1.component.ts
+++ src/app/invoice-module/invoice-module1/invoice-module1.component.ts
@@ -52,12 +52,13 @@ projectName?:any = localStorage.getItem('projectName');
    
   }
 
-  viewCreditNote(projectId: any, po:any,sfdc:any, pa:any, totalManDays:any, projectName:any) {
+  viewCreditNote(projectId: any, po:any,sfdc:any, pa:any, totalManDays:any, projectName:any, clientAddress:any) {
     localStorage.setItem('projectId', projectId);
     localStorage.setItem('po', po);
     localStorage.setItem('sfdc',sfdc);
     localStorage.setItem('pa',pa);
     localStorage.setItem('totalManDays',totalManDays);
+    localStorage.setItem('clientAddress',clientAddress);
     console.log('project Name', this.projectName);
     localStorage.setItem('projectName', projectName);
     // this.api.getProjectDetails(projectId).subscribe((data) => {
