d2bce6faaf29b5177f3dbb4f0a6b4229cebbf316
Raga2707 <ragamalya295@gmail.com>
2023-06-12 10:45:24 +0530

    Modified environment.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,14 @@
+// export const environment = {
+//   production: true,
+//   url: 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/zaga/information',
+//   mailUrl:
+//     'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/sendmail',
+// };
 export const environment = {
-  production: true,
-  url: 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/zaga/information',
-  mailUrl:
-    'https://us-east-1.aws.data.mongodb-api.com/app/application-0-wouft/endpoint/sendmail',
+  production: false,
+  projectMgtUrl: 'http://localhost:8081/zaga/projectManagement',
+  invoiceUrl: 'http://localhost:8083/Zaga/Invoice',
+  employeeUrl: 'http://localhost:8090/zaga/employeeOnboarding/',
+  emailUrl: 'http://localhost:8084/emailNotification'
 };
+
