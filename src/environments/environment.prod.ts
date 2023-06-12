f467d9a2968eb9fc5f5d078520181354ba71c29f
Raga2707 <ragamalya295@gmail.com>
2023-06-12 14:09:49 +0530

    Added new URL in enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,9 +6,9 @@
 // };
 export const environment = {
   production: true,
-  projectMgtUrl: 'http://localhost:8081/zaga/projectManagement',
-  invoiceUrl: 'http://localhost:8083/Zaga/Invoice',
-  employeeUrl: 'http://localhost:8090/zaga/employeeOnboarding/',
-  emailUrl: 'http://localhost:8084/emailNotification'
+  projectMgtUrl: 'https://project-management-jvm.zaga-office-automation.svc.cluster.local:8081/zaga/projectManagement',
+  invoiceUrl: 'https://zaga-invoice-new-jvm.zaga-office-automation.svc.cluster.local:8083/Zaga/Invoice',
+  employeeUrl: 'https://onboarding-api-new.zaga-office-automation.svc.cluster.local:8090/zaga/employeeOnboarding/',
+  emailUrl: 'https://email-notification-jvm.zaga-office-automation.svc.cluster.local:8084/emailNotification'
 };
 
